0947b40fd2a452bcbc95cd57f780fb016b921497
Diego Pedrazas <diego.pedrazas@EOSchools.org>
2023-06-26 16:06:13 -0400

    agregando

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -2,6 +2,7 @@ const Error = ({children}:any) => {
   return (
     <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3">
         {children}
+        <p>Algo</p>
     </div>
   )
 }
